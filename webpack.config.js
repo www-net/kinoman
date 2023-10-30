e7a3c39c6c9b3f7e4e4f46b9e0ed64b7e3e6df95
www-net <78052177+www-net@users.noreply.github.com>
2023-10-30 13:01:38 +0300

    refactor: add moment.js and moment-locales-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = {
     mode: `development`, // Режим сборки
@@ -11,5 +12,8 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, `public`), // Где искать сборку
         watchContentBase: true,
-    }
+    },
+    plugins: [
+      new MomentLocalesPlugin()
+    ]
 }
