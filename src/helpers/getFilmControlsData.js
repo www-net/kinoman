02e88b2581a362dabac0a3f412e81a7a82fe7495
www-net <78052177+www-net@users.noreply.github.com>
2023-10-30 16:12:24 +0300

    feat: add dateTime helpers

---
+++ src/helpers/getFilmControlsData.js
@@ -0,0 +1,40 @@
+const controlsData = {
+  watchlist: {
+    key: `isInWatchList`,
+    variants: {
+      'no': `Add to watchlist`,
+      'yes': `Already in watchlist`,
+    }
+  },
+  watched: {
+    key: `isWatched`,
+    variants: {
+      'no': `Mark as watched`,
+      'yes': `Already watched`,
+    }
+  },
+  favorite: {
+    key: `isFavorite`,
+    variants: {
+      'no': `Add to favorites`,
+      'yes': `Already in favorites`,
+    }
+  },
+};
+
+// Собрать данные для контролов
+// входящийм объектом будет {isInWatchList, isWatched, isFavorite}
+export const getFilmControlsData = (data) => {
+  return Object.entries(controlsData)
+    .map(([id, controlData]) => {
+      const {key, variants} = controlData;
+      const isActive = data[key];
+      const text = isActive ? variants.yes : variants.no;
+
+      return {
+        id,
+        text,
+        isActive
+      };
+    });
+};
