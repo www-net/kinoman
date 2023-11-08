5f96e05d57b2349a15fdadd49a8683f76f6a2499
www-net <78052177+www-net@users.noreply.github.com>
2023-11-08 18:11:10 +0300

    refactor: remove some card flags from object props

--- src/components/card.js
+++ src/components/card.js
@@ -24,9 +24,6 @@ export default class Card {
     this.runtime = getRuntime(runtime);
     this.rating = rating;
     this.commentsCount = comments.length;
-    this.isInWatchList = isInWatchList;
-    this.isWatched = isWatched;
-    this.isFavorite = isFavorite;
 
     // Данные для контролов карточки
     this.controlsData = getFilmControlsData({
