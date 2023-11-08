3a90bb5e20090adb53ce95ca2075a7b21be422b9
www-net <78052177+www-net@users.noreply.github.com>
2023-11-08 19:47:54 +0300

    refactor: create filmsContainer element instead of querying it. And remove needless props from details

--- src/components/films-list.js
+++ src/components/films-list.js
@@ -12,9 +12,9 @@ export default class FilmsList {
     this.isUpcoming = this.type === `upcoming`;
 
     this.shownQuantity = 0;
-    this.elem = this.getSection();
-    this.filmsContainer = this.elem.querySelector(`.films-list__container`);
     this.ShowMoreBtn = this.getShowMoreBtn();
+    this.filmsContainer = createElement(`<div class="films-list__container"></div>`);
+    this.elem = this.getSection();
     this.addCards = this.addCards.bind(this);
 
     this.addCards();
@@ -104,13 +104,13 @@ export default class FilmsList {
     }
     const markup = `<section class="${this.getClassName()}">
     ${this.getTitle()}
-    <div class="films-list__container"></div>
   </section>`;
 
     const section = createElement(markup);
+    section.append(this.filmsContainer);
     section.append(this.ShowMoreBtn);
 
-    return createElement(markup);
+    return section;
   }
 
   getElement() {
