645443978a76509b5b8341c6caf9d75ca916e592
www-net <78052177+www-net@users.noreply.github.com>
2024-04-26 18:08:21 +0300

    feat: add Film controller; update Card & Details components to use it. WIP

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -1,4 +1,4 @@
-import AbstractComponent from '../abstract-component';
+import AbstractSmartComponent from '../abstract-smart-component';
 import CloseBtn from './close-btn';
 import Poster from './poster';
 import Desc from './desc';
@@ -6,9 +6,9 @@ import Head from './head';
 import Table from './table';
 import Controls from './controls';
 import Comments from '../comments';
-import {createElement, renderElement, removeElement} from '../../helpers';
+import {createElement, renderElement} from '../../helpers';
 
-export default class Details extends AbstractComponent {
+export default class Details extends AbstractSmartComponent {
   constructor(filmData) {
     super();
 
@@ -19,65 +19,67 @@ export default class Details extends AbstractComponent {
     this._table = new Table(filmData);
     this._comments = new Comments(filmData);
     this._controls = new Controls(filmData);
-
-    this._hideDetails = this._hideDetails.bind(this);
-    this._closeBtn.setClickHandler(this._hideDetails);
+    this._chosenEmoji = null;
   }
 
-  _hideDetails() {
-    removeElement(this);
+  setEmojiClickHandler(handler) {
+    this._comments.setEmojiClickHandler(handler);
   }
+
+  setCloseBtnClickHandler(handler) {
+    this._closeBtn.setClickHandler(handler);
+  }
+
+  setControlsClickHandler(handler) {
+    this._controls.setClickHandler(handler);
+  }
+
+  _recoveryListeners() {}
 
   _getInfo() {
     const element = createElement(`<div class="film-details__info"></div>`);
-    renderElement(element, [this._head, this._table, this._desc]);
+
+    renderElement(element, [
+      this._head,
+      this._table,
+      this._desc
+    ]);
 
     return element;
   }
-
   _getInfoContainer() {
     const element = createElement(`<div class="film-details__info-wrap"></div>`);
     renderElement(element, [
       this._poster,
       this._getInfo()
     ]);
-
     return element;
   }
-
   _getTopContainer() {
     const element = createElement(`<div class="form-details__top-container"></div>`);
-
     renderElement(element, [
       this._closeBtn,
       this._getInfoContainer(),
       this._controls
     ]);
-
     return element;
   }
-
   _getForm() {
     const element = createElement(`<form class="film-details__inner" action="" method="get"></form>`);
-
     renderElement(element, [
       this._getTopContainer(),
       this._comments
     ]);
-
     return element;
   }
-
   _getTmpl() {
     return (
       `<section class="film-details"></section>`
     );
   }
-
   _createElement() {
     const element = createElement(this._getTmpl());
     renderElement(element, this._getForm());
-
     return element;
   }
 }
