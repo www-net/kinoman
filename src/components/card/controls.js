8d92034d7de8c2d06285d8794be2a9b51090ab7b
www-net <78052177+www-net@users.noreply.github.com>
2024-04-26 17:35:30 +0300

    feat: add setClickHandler() method to card Controls component. WIP

--- src/components/card/controls.js
+++ src/components/card/controls.js
@@ -1,38 +1,59 @@
 import AbstractComponent from '../abstract-component';
-
 import {getClass, getFilmControlsData} from '../../helpers';
-
 export default class Controls extends AbstractComponent {
   constructor({isInWatchList, isWatched, isFavorite}) {
     super();
-
     this._controlsData = getFilmControlsData({
       isInWatchList,
       isWatched,
       isFavorite,
     });
   }
-  _getCardControl({id, text, isActive}) {
+
+  _getClickHandler(handler) {
+    return (event) => {
+      const control = event.target.closest(`.film-card__controls-item`);
+
+      if (!control) {
+        return;
+      }
+
+      const {prop} = control.dataset;
+
+      if (!prop) {
+        return;
+      }
+
+      handler(prop);
+    };
+  }
+
+  setClickHandler(handler) {
+    this.getElement().addEventListener(`click`, this._getClickHandler(handler));
+  }
+
+  _getCardControl({id, key, text, isActive}) {
     const mods = [id];
 
     if (isActive) {
       mods.push(`active`);
     }
-
     const className = getClass({
       base: `film-card__controls-item`,
       mods
     });
 
     return (
-      `<button class="${className}">${text}</button>`
+      `<button
+        class="${className}"
+        data-prop="${key}"
+      >${text}</button>`
     );
   }
 
   _getTmpl() {
     const controlsMarkup = this._controlsData
       .reduce((prev, control) => prev + this._getCardControl(control), ``);
-
     return (
       `<form class="film-card__controls">
         ${controlsMarkup}
