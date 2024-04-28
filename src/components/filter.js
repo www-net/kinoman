a845f4b9a19ef4c44fa686bfa70da1562ebb006c
www-net <78052177+www-net@users.noreply.github.com>
2024-04-28 10:33:41 +0300

    refactor: move filters data to constants

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,4 +1,6 @@
 import AbstractComponent from './abstract-component';
+import {FILTERS} from '../constants';
+
 const classes = {
   default: `main-navigation__item`,
   active: `main-navigation__item--active`
@@ -11,29 +13,6 @@ export default class Filter extends AbstractComponent {
     this._data = filmsData;
     this._defaultFilter = `all`;
     this._currentFilter = currentFilter || this._defaultFilter;
-
-    this._sections = [
-      {
-        id: `all`,
-        key: `all`,
-        name: `All movies`
-      },
-      {
-        id: `watchlist`,
-        key: `isInWatchList`,
-        name: `Watchlist`
-      },
-      {
-        id: `history`,
-        key: `isWatched`,
-        name: `History`
-      },
-      {
-        id: `favorites`,
-        key: `isFavorite`,
-        name: `Favorites`
-      },
-    ];
   }
 
   setCurrentFilter(filter) {
@@ -55,6 +34,7 @@ export default class Filter extends AbstractComponent {
     this._currentControl = this.getElement().querySelector(`.${classes.default}--all`);
     this._currentControl.classList.add(classes.active);
   }
+
   _createHandler(handler) {
     return (event) => {
       const control = event.target.closest(`.${classes.default}`);
@@ -62,13 +42,16 @@ export default class Filter extends AbstractComponent {
       if (!control) {
         return;
       }
+
       const {filterProp} = control.dataset;
       if (filterProp === this._currentFilter) {
         return;
       }
+
       if (!this._currentControl) {
         this._currentControl = this.getElement().querySelector(`.${classes.active}`);
       }
+
       this._currentControl.classList.remove(classes.active);
       control.classList.add(classes.active);
       this._currentControl = control;
@@ -76,19 +59,22 @@ export default class Filter extends AbstractComponent {
       handler(filterProp);
     };
   }
+
   setClickHandler(handler) {
     this._clickHandler = this._createHandler(handler);
     this.getElement().addEventListener(`click`, this._clickHandler);
   }
+
   _getItems() {
-    return this._sections.reduce((prev, section) => {
-      const {id, key, name} = section;
+    return Object.entries(FILTERS).reduce((prev, item) => {
+      const [key, {id, name}] = item;
       let counter = 0;
       let counterMarkup = ``;
       let className = classes.default;
       className += ` ${classes.default}--${id}`;
+
       if (key !== `all`) {
-        counter = this._data.filter((item) => item[key]).length;
+        counter = this._data.filter((filmItem) => filmItem[key]).length;
         counterMarkup = `<span class="main-navigation__item-count">
           ${counter}
         </span>`;
@@ -105,6 +91,7 @@ export default class Filter extends AbstractComponent {
       );
     }, ``);
   }
+
   _getTmpl() {
     return (
       `<nav class="main-navigation">
