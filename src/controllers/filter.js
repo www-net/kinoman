d411205de7c10fb252702fbc76a891991a8a6a9b
www-net <78052177+www-net@users.noreply.github.com>
2024-05-07 12:33:24 +0300

    refactor: simplify Sort & Filter components & controllers

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -11,8 +11,10 @@ export default class FilterController {
     this._setFilterType = this._setFilterType.bind(this);
 
     this._onDataChange = this._onDataChange.bind(this);
+    this._onFilterChange = this._onFilterChange.bind(this);
 
     this._filmsModel.addDataChangeHandler(this._onDataChange);
+    this._filmsModel.addFilterChangeHandler(this._onFilterChange);
   }
 
   _setFilterType(filterType) {
@@ -21,6 +23,11 @@ export default class FilterController {
   }
 
   _onDataChange() {
+    this.render();
+  }
+
+  _onFilterChange() {
+    this._currentFilter = this._filmsModel.getFilterType();
     this.render();
   }
 
